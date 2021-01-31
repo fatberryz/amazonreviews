f63bfc0a1ede15792497dd63affe7f6f8950e5f5
fatberryz <ryantanwl@gmail.com>
2021-02-01 00:41:40 +0800

    working version of scrape into upload into GBQ
    Also added a TODO of resetting inputs/outputs after upload into GBQ

--- main.py
+++ main.py
@@ -273,29 +273,30 @@ if __name__ == "__main__":
     # read arguments for scraper
     args = parse_args()
 
-    # # create urls to scrape reviews and products from a csv containing product ASINs
-    # create_urls()
+    # create urls to scrape reviews and products from a csv containing product ASINs
+    create_urls()
 
-    # # Scrape reviews 
-    # # TODO: Update to include rotation for googlebots2.1 in the useragents (See documentation)
-    # get_reviews()
-    # get_outstanding_reviews()
-    # combine_reviews((args.output_dir + '/reviews'), (args.final_output + '/reviews'))
+    # Scrape reviews 
+    # TODO: Update to include rotation for googlebots2.1 in the useragents (See documentation)
+    get_reviews()
+    get_outstanding_reviews()
+    combine_reviews((args.output_dir + '/reviews'), (args.final_output + '/reviews'))
 
-    # # Scrape products
-    # # TODO: Update to include rotation for googlebots2.1 in the useragents (See documentation)
-    # get_products()
-    # get_outstanding_products()
-    # combine_products((args.output_dir + '/products'), (args.final_output + '/products'))
+    #  Scrape products
+    #  TODO: Update to include rotation for googlebots2.1 in the useragents (See documentation)
+    get_products()
+    get_outstanding_products()
+    combine_products((args.output_dir + '/products'), (args.final_output + '/products'))
 
-    # # Obtain profile urls from scraped reviews in raw
-    # get_profile_urls()
+    # Obtain profile urls from scraped reviews in raw
+    get_profile_urls()
 
-    # # Scrape profiles
-    # get_profiles()
-    # get_outstanding_profiles()
-    # combine_profiles((args.output_dir + '/profiles'), (args.final_output + '/profiles'))
-
+    # Scrape profiles
+    get_profiles()
+    get_outstanding_profiles()
+    combine_profiles((args.output_dir + '/profiles'), (args.final_output + '/profiles'))
+    
+    # Upload consolidated CSVs into GBQ
     upload_consolidated_csvs('./credential_file.json', 'crafty-chiller-276910', 'scraped_items_test' )
 
     ## TODO: CLEAR ALL OUTPUT / INPUT FOLDERS after upload
