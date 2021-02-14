21f294b7b5d161726fd2a646a55aa7ffc00f8208
fatberryz <ryantanwl@gmail.com>
2021-02-14 15:12:33 +0800

    update all item outputs to include date_scraped column
    updated blank csv files with appropriate headers so that error of wrong number of expected columns is resolved

--- stitch.py
+++ stitch.py
@@ -49,31 +49,50 @@ def combine_reviews(output_dir, final_dir):
 
 def combine_profiles(output_dir, final_dir):
     res_lst = glob.glob("{output_dir}/*.csv".format(output_dir=output_dir))
-    total_df = pd.DataFrame()
-
+    col_names = [
+        'json_data',
+        'acc_num',
+        'name',
+        'occupation',
+        'location',
+        'description',
+        'badges',
+        'ranking',
+        'date_scraped'
+    ]
+    total_df = pd.DataFrame(columns=col_names)
     for fn in res_lst:
         if is_non_zero_file(fn):
             curr_df = pd.read_csv(fn)
-            total_df = pd.concat([curr_df, total_df])
-
-    if os.path.isfile(f'{final_dir}/consolidated_profiles.csv'):
-        total_df = pd.concat([pd.read_csv(f'{final_dir}/consolidated_profiles.csv'), total_df])
-        total_df.drop_duplicates().to_csv("{final_dir}/consolidated_profiles.csv".format(final_dir=final_dir), index=False, mode='a', header=False)
-    else:
-        total_df.drop_duplicates().to_csv("{final_dir}/consolidated_profiles.csv".format(final_dir=final_dir), index=False)
+            # remove duplicates from multiple retries of outstanding profiles
+            curr_df.drop_duplicates(keep='first')
+            curr_df.columns = total_df.columns
+            total_df = pd.concat([curr_df, total_df], ignore_index=True)
+        # remove empty rows and extra rows containing column headers
+        total_df = total_df.dropna(subset=['acc_num'])
+        total_df = total_df[total_df.json_data != 'json_data']
+        total_df.drop_duplicates(keep='first').to_csv("{final_dir}/consolidated_profiles.csv".format(final_dir=final_dir), index=False, header=col_names)
 
 
 def combine_products(output_dir, final_dir):
     res_lst = glob.glob("{output_dir}/*.csv".format(output_dir=output_dir))
-    total_df = pd.DataFrame()
-
+    col_names = [
+    'ASIN',
+    'availability',
+    'date_scraped',
+    'description',
+    'price',
+    'rating'
+    ]
+    total_df = pd.DataFrame(columns=col_names)
     for fn in res_lst:
         if is_non_zero_file(fn):
             curr_df = pd.read_csv(fn)
-            total_df = pd.concat([curr_df, total_df])
-
-    if os.path.isfile(f'{final_dir}/consolidated_products.csv'):
-        total_df = pd.concat([pd.read_csv(f'{final_dir}/consolidated_products.csv'), total_df])
-        total_df.drop_duplicates().to_csv("{final_dir}/consolidated_products.csv".format(final_dir=final_dir), index=False, mode='a', header=False)
-    else:
-        total_df.drop_duplicates(subset='ASIN').to_csv("{final_dir}/consolidated_products.csv".format(final_dir=final_dir), index=False)
+            # remove duplicates from multiple retries of outstanding products
+            curr_df.drop_duplicates(keep='first')
+            curr_df.columns = total_df.columns
+            total_df = pd.concat([curr_df, total_df], ignore_index=True)
+        # remove empty rows and extra rows containing column headers
+        total_df = total_df.dropna(subset=['ASIN'])
+        total_df = total_df[total_df.ASIN != 'ASIN']
+        total_df.drop_duplicates(keep='first').to_csv("{final_dir}/consolidated_products.csv".format(final_dir=final_dir), index=False, header=col_names)
